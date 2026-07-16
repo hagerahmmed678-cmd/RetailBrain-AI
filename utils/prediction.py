6cdd9b9e3bbd6f4724b90e90c0c43f9fd6702508
Hager <hajerahmmed525@gmail.com>
2026-07-16 03:21:26 +0300

    Update prediction.py

--- utils/prediction.py
+++ utils/prediction.py
@@ -1,1 +1,32 @@
 
+"""
+Prediction Utilities
+"""
+
+import tempfile
+
+
+def run_prediction(model, image):
+
+    """
+    Run YOLO inference on the uploaded image.
+    """
+
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=".jpg"
+    ) as temp:
+
+        image.save(temp.name)
+
+        results = model.predict(
+
+            source=temp.name,
+
+            conf=0.25,
+
+            verbose=False
+
+        )
+
+    return results[0]
